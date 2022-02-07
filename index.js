9c8c86c8f1a4011bd79dd72df065cdd1cdc9bad0
Ansel Alldredge <99053195+AnselA01@users.noreply.github.com>
2022-02-07 15:40:54 -0600

    using yfinance for enchange, added code for symbol search box

---
+++ index.js
@@ -0,0 +1,40 @@
+function saveData(symbol) {
+    var symbol = {
+      Symbol: symbol
+    };
+    symbol = symbol.Symbol;
+    localStorage.setItem('_symbol', symbol);
+}
+function getSymbol(){
+    var symbol = document.getElementById("symbolInput").value;
+    var url = "https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols=";
+    url = url.concat(symbol)
+    var config = {
+    method: 'get',
+    url: url,
+    headers: { 
+    'accept': 'application/json', 
+    'X-API-KEY': 'VmKAUWKlFm1sHGmUkQQqd1eacYPTTYfFKAAfflEe'
+    }
+};
+
+axios(config)  //receive data
+.then(function (response) {
+    //bid, ask, price
+    var exchange = response.data.quoteResponse.result[0].exchange;
+            if (exchange == "NYSE" || "NasdaqGS" || "OTC") {
+                document.location.href="stocks.html"
+            }
+            if (exchange == "binance") {
+                document.location.href="crypto.html"
+            }
+            if (exchange == "Forex") {
+                document.location.href="forex.html";
+            }
+        })
+    return symbol;
+}
+saveData(symbol);
+
+//exchange and where to direct user
+
