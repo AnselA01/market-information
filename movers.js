552b36bc930d812160c567de91e5b5bd8cafb415
Ansel Alldredge <99053195+AnselA01@users.noreply.github.com>
2022-03-13 20:44:20 -0500

    Added movers table to movers.html, movers.js needs to be configured to put data in the table.

--- movers.js
+++ movers.js
@@ -1,3 +1,5 @@
+//formula to find original value from percentage change: 
+//100 + percent change, divide 100 by it (100/x), then multiply by the last value to get the first value.
 function saveData(symbol) {
     var symbol = {
       Symbol: symbol
@@ -23,23 +25,23 @@ var accessTokenConfig = {
     headers: { 
   'Content-Type': 'application/x-www-form-urlencoded'
     }, 
-    data: "grant_type=refresh_token&refresh_token=sJZJw7RCDpjE6Pf1RX8dkje7C%2B6W4c71E7OB45ij1iQ9ixYFnPVf8V2FAwK1xvL4zHuqw%2Fz4ASdygVcB0dLHoAsau9HrTj3RGafRyUyQDIiRhk8xtUXYYxXC3KKaJbVefg6diunxKzGIq%2Fpmwk3UYKEbq3JXlqFwcnM5Nh6vjXn9Z8MA0Fk9hpRvDpBIg0UqqFN7wvCgBLmzQeZV39bld%2B%2FycC1q7HsLA18rG9gB7cT94QXu4XKhWAvebReNB4JevLXR8%2Fc0AsRiUdKWggYPeMjkQSI7eQYpOnJgH0i1zB8DEGrnqP%2Fe5jy8R2PFr4T1Q%2FSG1a1%2B%2BTZ%2BeEmz1I1sS7Wyy4gVrLYKERU67x71uJD4Rn3ofXX8GIXhgykpY8oBsQ%2BJrNHNt039KcVFfHrKtWacI0bj%2BZWIKeCKboGNks9nc%2B3cETJjmCO8Bwo100MQuG4LYrgoVi%2FJHHvlsX2BEUUjT4MT9JMKq4RUTjTMyN2BlGrqA7OU%2BKE5gv5e0V92FncXmSf8UXTMfVeD6xnmEdzub1nNmyo2nRbIRPJVob3xYjBe1bdsL7mmQD0ltfZdlTm85MHehQZXJtGuDZDQsyYYPzmn4n%2BQPZC8rRMKyrlAieLQEXBe7%2BZkahv9Mn7DvZZHrWivvcwICHwsJHCSD4jUnb0rfrK%2BEp9szl4igfXykVyMIRWblZHr7TVV8PtwdLSdXm7BKc0cPshQ6wPZVyoKULwPU05JSgnmSimEAnlS2PYpaFmv7NNnP45MK1kfve5ApUIcSxwd4CdIp2H8S2kqzy%2BddBh8e%2BXM9ih8KT5c4FyI%2B9xmHfFNxKAPq8yXso1L6DGdDxiRqT4dskV3OM9JptRjQ7QjnoARPEbzRCJUB90MwI6k9nqdfr%2FZbGMQPm%2BxZ%2BUIEKo%3D212FD3x19z9sWBHDJACbC00B75E&access_type=&code=&client_id=PBTASGIYTYGO8FI5QLXRZS63AXHG40XH%40AMER.OAUTHAP&redirect_uri=http%3A%2F%2Flocalhost"
+    data: "grant_type=refresh_token&refresh_token=XjcIadXu73isZmpyIZT15UhN3mNp9ILkK3gGy26P0cs9DfBaJKlZqMoCxfuv5WjK0WoSd6%2B6xeprq2kM3qGNfBP3Cpcim38Mk%2FlsSC0fknzqDtJixmfKgqJBge8gv9RvUBd5ac%2BarfoijZguw00IwQH9Gl4yPbcQmE%2FaH8BV9YEDkpo2NrCcX5BON7M0t9XHFsuNhHT0yUq7Qsb7DZeOJGP%2BEysq%2FJ9fDeavKSkv45mt%2BgtA2u2ZULeltj7WR2Bkk8fGS6%2BZc9a%2Fccg%2BzIjowoyL36O1yMpZp%2FnbCNwepfgwT%2B9oXDN73NmNXdBx2bQSO8DsyN354S28Ye%2F%2B2vPAxixRdNZZ7HycpDs4HOeYOMdErR4Na9tMINN7D4Q%2BFOkqwYtCLb91CQuH3paL0sNWW0ZFzm6QbqexAFTe59AhAMCBJ85Wlg9pk1%2BqjqD100MQuG4LYrgoVi%2FJHHvlAZDqws0AsekPNvgb%2FLm9x6UdtaKtz9kcH9b%2FHFkjWThiatgCiEg0XA%2Fl%2FazTCx1YccujE0M2%2BXrdhReqaGX34ZJEoOfSh0MzRA%2BoMJhqHJoiEAid0ycT6Hf9TQ7pIsYhaCQA5dUDfJZ%2B%2FD2AUvmefxzQ4mbFiZcNVPLIOGMzSPY9X0T8Gwg69DId%2Fi6mQLiG6axVsIejer%2BaR3x6GmunBcO4SpcWroyHHZQkPnmc2AH4d9HGvkM33ANMZP38ybfnPgeZUoZX6vO%2BFPFYP%2BFw%2FOnpivZYJpsAzQLZYGNIxB7MZqVmLvX4mpPbsT7vXo5NlbrmNvQLNkiHYBbbtZA2ufJfppDUB4Rw%2FIrR3Ogkz%2F%2F6Qw3ckBDgFSzYQqm6yPB8IBjlUCFc5uRWTtFAjqL2E1DTpS6j%2FDEa%2BKbAze2%2FYzqC6XnFbzXXv6K%2BPY4%3D212FD3x19z9sWBHDJACbC00B75E&access_type=&code=&client_id=PBTASGIYTYGO8FI5QLXRZS63AXHG40XH%40AMER.OAUTHAP&redirect_uri="
   };
   
   axios(accessTokenConfig)
   .then(function (response) {
-    var accessToken = "Bearer " + response.data.access_token;
+    var accessToken = response.data.access_token;
     
     var moversConfig = {
       method: 'get',
       url: 'https://api.tdameritrade.com/v1/marketdata/$SPX.X/movers?apikey=PBTASGIYTYGO8FI5QLXRZS63AXHG40XH&change=percent',
       headers: { 
-        'Authorization': accessToken,
+        'Authorization': "Bearer" + accessToken,
       }
     };
     
     axios(moversConfig)
     .then(function (response) {
-      console.log(response.data);
+      console.log(response);
     })
   })
