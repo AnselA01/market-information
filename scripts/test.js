751169ab0ac73bf10a2a08497bc67789af12d308
Ansel Alldredge <99053195+AnselA01@users.noreply.github.com>
2022-03-19 20:46:19 -0500

    Delete test.js

--- scripts/test.js
+++
@@ -1,9 +0,0 @@
-var yahooFinance = require('yahoo-finance');
-
-yahooFinance.quote({
-  symbol: 'AAPL',
-  modules: ['price', 'summaryDetail']
-}, function (err, quote) {
-  var price = (quote.price.regularMarketPrice);
-  console.log(price);
-})
